99be4bd6f32fc13e33a82e2a054276f015410bba
daikou1928 <daikou1928@gmail.com>
2024-09-01 11:23:59 +0900

    preview

--- src/select.jsx
+++ src/select.jsx
@@ -2,14 +2,16 @@ import React,{useState,useEffect} from "react";
 import styles from './main.css';
 function Select(){
     const[select,setSelect] = useState(Array(32).fill(0));
-    var items = [];
+    var items = [<tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+    <td>曜日</td><td>駅出発</td><td>開始</td><td>終了</td><td>帰宅</td></tr>];
     const go = ["-","5:36","6:09","6:09","10:03","11:11","11:11","19:38"];
     const sta = ["-","6:30","7:00","7:00","11:00","12:00","12:00","21:00"];
     const end = ["-","15:30","16:00","16:00","20:00","21:00","21:00","翌7:00"];
     const home = ["-","16:20","16:59","16:59","21:04","21:53","21:53","翌7:55"];
     var tbl = [];
     var days = [];
-    let scr = [];
+    var you = ["昼","月","火","水","木","金","土"];
+    var you_pri = ["日","月","火","水","木","金","土"]
     const chan = function(n,v){
         let bebut = document.getElementById(`${n}${select[n]}`);
         bebut.style.backgroundColor = "rgb(206, 201, 201)";
@@ -27,7 +29,7 @@ function Select(){
     for(let i = 0;i<31;i++){
         items.push(
         <tr key = {i}>
-        <td>{i+1}</td><td><button onClick={()=>chan(i,1)} id={`${i}1`} >A</button></td>
+        <td>{i+1}日</td><td><button onClick={()=>chan(i,1)} id={`${i}1`} >A</button></td>
         <td><button onClick={()=>chan(i,2)}id={`${i}2`}>B</button></td>
         <td><button onClick={()=>chan(i,3)}id={`${i}3`}>C</button></td>
         <td><button onClick={()=>chan(i,4)}id={`${i}4`}>D</button></td>
@@ -35,6 +37,11 @@ function Select(){
         <td><button onClick={()=>chan(i,6)}id={`${i}6`}>F</button></td>
         <td><button onClick={()=>chan(i,7)}id={`${i}7`}>夜</button></td>
         <td><button onClick={()=>chan(i,0)}id={`${i}0`} class="non">なし</button></td>
+        <td className = "prev">{you_pri[(select[31]+i)%7]}</td>
+        <td className = "prev">{go[select[i]]}</td>
+        <td className = "prev">{sta[select[i]]}</td>
+        <td className = "prev">{end[select[i]]}</td>
+        <td className = "prev">{home[select[i]]}</td>
         </tr>);
         console.log(items[i]);
     }
@@ -48,7 +55,6 @@ function Select(){
         <td><input type = "radio" name = "31"value = "6"onChange={()=>chang(6)}/>土</td>
         <td><input type = "radio" name = "31"value = "0"onChange={()=>chang(0)}/>日</td>
         </tr>);
-    var you = ["昼","月","火","水","木","金","土"];
     for(let j = 0;j<31;j++){
         console.log((select[31]+j)%7,select[j]);
         if((select[31]+j)%7>=1 && (select[31]+j)%7<=5){
@@ -76,26 +82,18 @@ function Select(){
     return(
         <div className = "Select">
             <div className = "scr">
-            <table>
                 {days}
-            </table>
             </div>
-            <table>
-                <tr><td>
             <div className = "scr">
             <table>
             {items}
             </table>
             </div>
-            </td><td>
+            <button className = "print"onClick={()=>window.print()}>印刷</button>
             <table className = "prt">
                 <tr><th>日付</th><th>曜日</th><th>駅出発</th><th>開始</th><th>終了</th><th>帰宅</th><th className="memo">メモ</th></tr>
             {tbl}
             </table>
-            </td>
-            </tr>
-            </table>
-            <button className = "print"onClick={()=>window.print()}>印刷</button>
         </div>
     )
 }
